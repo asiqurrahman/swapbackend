0570c570409996c3b50b87df88dfeca5377bf57f
asiqurrahman <asiqurrahman13@gmail.com>
2021-12-08 05:43:49 -0500

    staic files fixed

--- swap/settings.py
+++ swap/settings.py
@@ -77,6 +77,7 @@ SIMPLE_JWT = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -155,6 +156,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+STATIC_ROOT = BASE_DIR /'staticfiles'
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
