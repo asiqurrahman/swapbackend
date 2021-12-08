bae8291dd57eb6e612161b667af907cf7b375893
asiqurrahman <asiqurrahman13@gmail.com>
2021-12-08 05:36:58 -0500

    deployment

--- swap/settings.py
+++ swap/settings.py
@@ -13,9 +13,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q4hb6rmg)nvpg3u%z18bc*)+_q_s8l!4llp$c(@n0t9*0af=_q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://asiqursswap.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
