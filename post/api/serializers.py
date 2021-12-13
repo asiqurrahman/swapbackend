92686aca9a28f148d5b1c175eb1d14cacef88501
asiqurrahman <asiqurrahman13@gmail.com>
2021-12-13 03:17:16 -0500

    nested serializers fixed

--- post/api/serializers.py
+++ post/api/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 from post.models import Post # If used custom user model
+from user.api.serializers import UserSerializer
 
 class PostSerializer(serializers.ModelSerializer):
+    author_avatar = serializers.ImageField(source='author.avatar')
+    author_username = serializers.CharField(source='author.username')
     class Meta:
         model = Post
         # Tuple of serialized model fields (see link [2])
