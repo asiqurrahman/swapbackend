a29dfbbd4d32d75b189b57f2629acb0e97478765
asiqurrahman <asiqurrahman13@gmail.com>
2021-12-21 09:42:10 -0500

    post model update

--- post/models.py
+++ post/models.py
@@ -9,11 +9,10 @@ from django.utils import timezone
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=CASCADE)
     trade = models.CharField(max_length=100)
-    trade_description = models.TextField(null=True)
     wanted = models.CharField(max_length=100)
-    wanted_description = models.TextField(null=True)
     trade_image = models.ImageField()
     wanted_image = models.ImageField()
+    description = models.TextField(null=True)
     date_posted = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
